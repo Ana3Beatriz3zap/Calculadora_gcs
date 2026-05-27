accf61933b1394ffaefdc00901b8313baab50ead
Lopeszs <isabelafl07005@gmail.com>
2026-05-27 07:24:53 -0400

    feat: implementa operações de estatística (média, mediana e desvio padrão)

---
+++ calc_estatistica .py
@@ -0,0 +1,10 @@
+import statistics
+
+def media(lista):
+    return statistics.mean(lista)
+
+def mediana(lista):
+    return statistics.median(lista)
+
+def moda(lista):
+    return statistics.mode(lista)
