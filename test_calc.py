d30b70326981808c3c9bd455fa42ec45845d781a
Lopeszs <isabelafl07005@gmail.com>
2026-05-27 08:47:59 -0400

    test: implementação dos testes do módulo de estatística

--- test_calc.py
+++ test_calc.py
@@ -1,1 +1,22 @@
-#Arquivo - contendo os testes unitários para cada módulo da calculadora
+#Arquivo - contendo os testes unitários para cada módulo da calculadoraimport unittest
+import calc_estatistica
+
+class TestEstatistica(unittest.TestCase):
+
+    def test_media(self):
+        self.assertEqual(
+            calc_estatistica.media([2, 4, 6, 8]), 5
+        )
+
+    def test_mediana(self):
+        self.assertEqual(
+            calc_estatistica.mediana([1, 3, 5]), 3
+        )
+
+    def test_moda(self):
+        self.assertEqual(
+            calc_estatistica.moda([1, 2, 2, 3]), 2
+        )
+
+if __name__ == '__main__':
+    unittest.main()
