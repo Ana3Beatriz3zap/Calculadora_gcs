539c5cb8143300b52fb610fb0d9c142df4ea7ee9
amanda.jesus <amanda.jesus@softplan.com.br>
2026-05-26 20:15:49 -0400

    feat: implementa operações de percentual (percentual, acrescimo, desconto)

--- test_calc.py
+++ test_calc.py
@@ -1,1 +1,20 @@
-#Arquivo - contendo os testes unitários para cada módulo da calculadora
+"""Arquivo contendo os testes unitários da calculadora."""
+
+import unittest
+
+import calc_percentual
+
+
+class TestCalcPercentual(unittest.TestCase):
+	def test_percentual(self):
+		self.assertEqual(calc_percentual.percentual(200, 10), 20)
+
+	def test_acrescimo(self):
+		self.assertEqual(calc_percentual.acrescimo(200, 10), 220)
+
+	def test_desconto(self):
+		self.assertEqual(calc_percentual.desconto(200, 10), 180)
+
+
+if __name__ == "__main__":
+	unittest.main()
